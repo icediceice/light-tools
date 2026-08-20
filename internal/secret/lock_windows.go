0e86c597674ba6fcff0883d6ae167d813bdd54cd
icediceice <icediceice@users.noreply.github.com>
2026-08-20 07:47:25 +0000

    checkpoint: write internal/secret/lock_windows.go
    Light-Plan: 1539867042457329685

---
+++ internal/secret/lock_windows.go
@@ -0,0 +1,19 @@
+//go:build windows
+
+package secret
+
+import (
+	"os"
+
+	"golang.org/x/sys/windows"
+)
+
+func platformLock(file *os.File) error {
+	var overlapped windows.Overlapped
+	return windows.LockFileEx(windows.Handle(file.Fd()), windows.LOCKFILE_EXCLUSIVE_LOCK, 0, 1, 0, &overlapped)
+}
+
+func platformUnlock(file *os.File) error {
+	var overlapped windows.Overlapped
+	return windows.UnlockFileEx(windows.Handle(file.Fd()), 0, 1, 0, &overlapped)
+}
